2f12431b09c9f7bb59456e62ed16b49d0b2c23cb
ramthe666 <ramkumarshri30@gmail.com>
2021-11-29 18:10:35 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Anime Blog Stacks&trade;</h1>
+      <h2>Anime</h2>
+      <h3>Anime Blog</h3>
+      <h4>Animeya</h4>
+      <h5>Animeda</h5>
+      <h6>Anime always</h6>
+      <p>Anime Blog StacksWhen it comes to raising funds, a team’s specific needs should be front and center--not an afterthought. There’s no step more important in a project’s journey. It’s oftentimes the first interaction with their future community and can set the tone, for better or worse, of what’s to come.</p>
+      <p>coming zoooon</p>
+      <img src="https://cdn.mwallpapers.com/photos/wallpapers/anime/itachi-uchiha-android-iphone-desktop-hd-backgrounds-wallpapers-1080p-4khd-wallpapers-desktop-background-android-iphone-1080p-4k-lg1w3.jpg" alt="uchiha" />
+      <button>open</button>
+      <logo>gif</logo>
     </div>
   );
 }
