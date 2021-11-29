a12a28e8f31c720bd83b222d248831e9880daba8
ramthe666 <ramkumarshri30@gmail.com>
2021-11-29 22:36:20 +0530

    input-button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+  const [name,setToname] = useState("");
+
+  const nameType =()=>{
+    document.write("Your name is =", name);
+  }
+
+
   return (
     <div className="App">
       <h1>Anime Blog Stacks&trade;</h1>
@@ -13,7 +20,16 @@ function App() {
       <p>Anime Blog StacksWhen it comes to raising funds, a team’s specific needs should be front and center--not an afterthought. There’s no step more important in a project’s journey. It’s oftentimes the first interaction with their future community and can set the tone, for better or worse, of what’s to come.</p>
       <p>coming zoooon</p>
       <img src="https://cdn.mwallpapers.com/photos/wallpapers/anime/itachi-uchiha-android-iphone-desktop-hd-backgrounds-wallpapers-1080p-4khd-wallpapers-desktop-background-android-iphone-1080p-4k-lg1w3.jpg" alt="uchiha" />
-      <button>open</button>
+   <br/>   <label> Name : </label> <input
+id="addressid"
+  type='text'
+  placeholder='Enter Your Name'
+  name="toaddress"
+  required
+  onChange={event => setToname( event.target.value)}
+  
+/><br/>
+      <button onClick={()=>nameType()}>open</button>
       <logo>gif</logo>
     </div>
   );
